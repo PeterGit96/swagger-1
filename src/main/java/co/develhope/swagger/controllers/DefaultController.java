77f467df63b397d64df453947bfe5e8d04643144
Pietro Gallina <pietrogallina96@gmail.com>
2023-02-11 20:05:57 +0100

    Initial commit

---
+++ src/main/java/co/develhope/swagger/controllers/DefaultController.java
@@ -0,0 +1,16 @@
+package co.develhope.swagger.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class DefaultController {
+
+    @GetMapping
+    public String welcomeMsg() {
+        return "Hello, thanks for visiting us!";
+    }
+
+}
